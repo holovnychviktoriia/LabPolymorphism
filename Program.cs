548f01e5a13856628413fa1e202bd4f65931166d
Viktoriia Holovnych <c.holovnych.viktoriia@student.uzhnu.edu.ua>
2026-02-15 20:52:14 -0800

    add testimg vehice logic

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using LabPolymorphism;
+
+class Program
+{
+    static void Main()
+    {
+        List<Vehicle> vehicles = new List<Vehicle>
+        {
+            new Car("Audi", 180),
+            new Bicycle("Giant", 25),
+            new Airplane("Boeing 747", 900)
+        };
+
+        foreach (var vehicle in vehicles)
+        {
+            vehicle.Move();
+
+            if (vehicle is IRefuelable refuelable)
+            {
+                refuelable.Refill();
+            }
+
+            Console.WriteLine("---");
+        }
+    }
+}
